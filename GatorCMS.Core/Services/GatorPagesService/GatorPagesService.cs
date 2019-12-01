c89eec8eb4e276faa3516bdfb194dac23b4db066
David Poots <6442922+Donkeymasher@users.noreply.github.com>
2019-12-01 12:27:51 +0000

    removed uneeded code

--- GatorCMS.Core/Services/GatorPagesService/GatorPagesService.cs
+++ GatorCMS.Core/Services/GatorPagesService/GatorPagesService.cs
@@ -78,11 +78,5 @@ namespace GatorCMS.Core.Services.GatorPagesService
         {
             throw new NotImplementedException();
         }
-
-        private static T GetTfromString<T>(string mystring)
-        {
-            var foo = TypeDescriptor.GetConverter(typeof(T));
-            return (T)(foo.ConvertFromInvariantString(mystring));
-        }
     }
 }
